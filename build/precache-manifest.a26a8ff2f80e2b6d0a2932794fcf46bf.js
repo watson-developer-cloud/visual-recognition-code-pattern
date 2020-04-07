22bfb592746a79d207dd2d8ad9a5665c6911fc94
Mark Sturdevant <mark.sturdevant@ibm.com>
2020-04-07 10:28:56 -0700

    chore: Update dependencies (#11)
    * Update dependencies
    
    * Add build/static and build/precache-manifest

---
+++ build/precache-manifest.a26a8ff2f80e2b6d0a2932794fcf46bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e19793cd8b019eee5f1dc6becef761",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa3120f5becb6f6e1e96",
+    "url": "/static/css/main.db567354.chunk.css"
+  },
+  {
+    "revision": "b015196acd40f1ea70fc",
+    "url": "/static/js/2.6dbcdc8b.chunk.js"
+  },
+  {
+    "revision": "0795701ba7555fb0a8a6d3507fdf4a12",
+    "url": "/static/js/2.6dbcdc8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa3120f5becb6f6e1e96",
+    "url": "/static/js/main.5d5e26da.chunk.js"
+  },
+  {
+    "revision": "673936ebf47b427c979c",
+    "url": "/static/js/runtime-main.533ca349.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d140d7f91c9496b466e4d7fe645d3d1.js"
+  "/precache-manifest.a26a8ff2f80e2b6d0a2932794fcf46bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
